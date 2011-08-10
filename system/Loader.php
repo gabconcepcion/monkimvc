f024f619b65bf6c473f4bea457753a86a7a48818
Enrique Bondoc <enrique.p.bondoc@gmail.com>
2011-08-11 01:54:31 +0800

    Added static members autoLoad and _toClassFile - these members make this class ready if you want to utilize the spl_autoload_* family.

--- system/Loader.php
+++ system/Loader.php
@@ -2,6 +2,17 @@
 
 class Loader
 {
+	
+	/**
+	 *	Assuming you're following ZF
+	 */
+	const NAMESPACE_SEPARATOR = '_';
+	
+	/**
+	 *	Assuming you're only catering .php extension.
+	 */
+	const FILE_EXTENSION = '.php';
+	
 	function loadController($controller)
 	{
 		include('/applications/controllers/'.$controller.'.php');
@@ -12,18 +23,54 @@ class Loader
 	
 	function loadClass($class, $params=array())
 	{
-		$className = ltrim($class, '\\');
-		$file      = '';
-		$namespace = '';
-		if ($lastNsPos = strripos($className, '\\')) {
+		
+		if( empty( $class ) ) {
+			/**
+			 *	Much better if you place an exception handler here, add it on stack. Until I know
+			 *	your directory structure, I'll be adding a separate exception class.
+			 */
+			return;
+		}
+		
+		$className = ltrim($class, '\\');		
+		$file = $namespace = '';
+		if( $lastNsPos = strripos($className, '\\') ) {
 		    $namespace = substr($className, 0, $lastNsPos);
 		    $className = substr($className, $lastNsPos + 1);
 		    $file      = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
 		}
 		
 		$file = str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-		include('/applications/models/'.$file);
+		if( isset( $params['strict'] ) && true == $params['strict'] ) {
+			require_once( '/applications/models/' . $file );
+		} else {
+			include('/applications/models/'.$file);
+		}		
 	}
+	
+	public static final function autoLoad( $className )
+	{
+		if( empty( $className ) ) {
+			require_once( 'Loader' . DIRECTORY_SEPARATOR . 'Exception.php' );
+			throw new Loader_Exception( 'Class cannot be loaded - no class name given.' );
+		}
+		
+		$paths = explode( PATH_SEPARATOR, get_include_path() );		
+		if( empty( $paths ) ) {
+			require_once( 'Loader' . DIRECTORY_SEPARATOR . 'Exception.php' );
+			throw new Loader_Exception( 'Loading aborted - how come you have an empty include path?' );
+		}
+		
+		$baseFile = self::_toClassFile( $className ) . self::FILE_EXTENSION;
+		
+		foreach( $paths as $path ) {
+			$targetFile = $path . DIRECTORY_SEPARATOR . $baseFile;
+			if( file_exists( $targetFile ) ) {
+				return require_once( $targetFile );
+			}
+		}
+	}
+	
 	
 	function loadModel($model, $params=array())
 	{
@@ -57,5 +104,19 @@ class Loader
 		
 		$controller = get_class($oController);
 		include("/applications/views/{$controller}/$action.php");
+	}
+	
+	private static function _toClassFile( $className )
+	{
+		if( empty( $className ) ) {
+			require_once( 'Loader' . DIRECTORY_SEPARATOR . 'Exception.php' );
+			throw new Loader_Exception( 'Class cannot be loaded - no class name given.' );
+		}
+		
+		if( false !== strpos( $className, self::NAMESPACE_SEPARATOR ) ) {
+			return $className;
+		}
+		
+		return str_replace( self::NAMESPACE_SEPARATOR, DIRECTORY_SEPARATOR, trim( $className ) );
 	}
 }
