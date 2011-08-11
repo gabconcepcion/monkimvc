b16bd975cb7b9fe14187f073ec0587348ab7f96d
ilovekhym <concepciongabriel@yahoo.com>
2011-08-11 09:56:12 +0800

    * make sure private View properties are not available on view rendering

--- applications/views/home/index.php
+++ applications/views/home/index.php
@@ -3,4 +3,8 @@
 echo '<br/>From applications/views/home/index.php:<br/>';
 echo 'data:'.$data.'<br/>';
 echo 'admin_constant:'.$admin_constant.'<br/>';
-echo 'fruits: '.implode(', ', $fruits).'<br/>';   
+echo 'fruits: '.implode(', ', $fruits).'<br/>';  
+
+echo '<br/>Check if private properties of View class will also be available on this view file<br/>';
+echo '_private: '.$_private.'<br/>';   
+echo '_public: '.$_public.'<br/>';   
