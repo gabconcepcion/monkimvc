514b81e25511de2d0282a4c80f8eccc42390e2d8
ilovekhym <concepciongabriel@yahoo.com>
2011-08-11 09:54:47 +0800

    + test data

--- applications/Bootstrap.php
+++ applications/Bootstrap.php
@@ -5,7 +5,7 @@ $oAuth = new Auth_Model();
 
 echo 'From Bootstrap.php:<br/>';
 if(!$oAuth->hasIdentity())
-	echo 'you are not logged in.<br/>';
+	echo 'Auth Class: you are not logged in.<br/>';
 
 if(!$oAuth->isAdmin())
-	echo 'you are not an admin.<br/>';
+	echo 'Auth Class: you are not an admin.<br/>';
