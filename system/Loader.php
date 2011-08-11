a645ac8021eece2c86e6b315f6a65186ba6f55e1
ilovekhym <concepciongabriel@yahoo.com>
2011-08-11 09:50:51 +0800

    * fix the loadModel and remove not related method

--- system/Loader.php
+++ system/Loader.php
@@ -10,6 +10,11 @@ class Loader
 		return $oController;
 	}
 	
+        // From ZF  
+        // Autodiscover the path from the class name
+        // Implementation is PHP namespace-aware, and based on
+        // Framework Interop Group reference implementation:
+        // http://groups.google.com/group/php-standards/web/psr-0-final-proposal
 	function loadClass($class, $params=array())
 	{
 		$className = ltrim($class, '\\');
@@ -23,39 +28,17 @@ class Loader
 		
 		$file = str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 		include('/applications/models/'.$file);
+		
+		return array('namespace'=>$namespace,'className'=>$className,'file'=>$file,);
 	}
 	
-	function loadModel($model, $params=array())
+	static function loadModel($model, $params=array())
 	{
-		/** loadClass() copy - Need to use this for now to prevent error when calling as Loader::loadModel()*/
-		$class = $model;
-		$className = ltrim($class, '\\');
-		$file      = '';
-		$namespace = '';
-		if ($lastNsPos = strripos($className, '\\')) {
-		    $namespace = substr($className, 0, $lastNsPos);
-		    $className = substr($className, $lastNsPos + 1);
-		    $file      = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-		}
+		$oThis = new self();
+		$aClassInfo = $oThis->loadClass($model, $params);
 		
-		$file = str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-		include('/applications/models/'.$file);
-		/** END loadClass() copy*/
-		
+		$model = $aClassInfo['className'];
 		$oModel = new $model($params);
 		return $oModel;
-	}
-	
-	function renderView(Controller $oController, $action)
-	{
-		$aVars = get_object_vars($oController->view);
-		
-		foreach($aVars as $key=>$val)
-		{
-			$$key = $val;
-		}
-		
-		$controller = get_class($oController);
-		include("/applications/views/{$controller}/$action.php");
 	}
 }
